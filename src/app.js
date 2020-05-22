7962b0b912b8032516047af374f215e5aa3563e6
JMJ Riyana <jeemariyana@gmail.com>
2020-05-23 00:54:44 +0530

    code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('<h1>Hello World</h1>');
 })
  
-app.listen(3000)
+app.get('/about', function (req, res) {
+    res.send('<h1>Hello World</h1>');
+  })
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000")
+})
