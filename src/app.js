4ce68282603a006661fccdb480e9ec90e09d8168
JMJ Riyana <jeemariyana@gmail.com>
2020-05-23 01:38:12 +0530

    code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.use(express.static(publicDirectoryPath));
 app.set('view engine', 'hbs');
  
 app.get('/', function (req, res) {
