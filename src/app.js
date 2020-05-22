9189c8471ce34cd53044e49542d23f567a7dc259
JMJ Riyana <jeemariyana@gmail.com>
2020-05-23 01:01:28 +0530

    code

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
  
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>');
+  res.send('<h1>Hello cccWorld</h1>');
 })
  
 app.get('/about', function (req, res) {
-    res.send('<h1>Hello World</h1>');
+    res.send('<h1>Hello xxxWorld</h1>');
   })
 
 app.listen(3000, () => {
